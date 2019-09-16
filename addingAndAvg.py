1d4efd786578d97b70fcadf33893523c1e60f3d6
Elderied McKinney <mckinneydemetrice@icloud.com>
2019-09-16 01:18:13 -0500

    added a function for avg, and suming
    added funcrtion to add and another function to take avgerages

---
+++ addingAndAvg.py
@@ -0,0 +1,59 @@
+def addingSum(a,b):
+    return a + b
+
+
+
+addingSum(3,4)
+
+
+
+print(addingSum(4,5))
+
+def avgSum(totalnumAdd):
+    
+    for idex in totalnumAdd:
+        
+        total = sum(totalnumAdd)
+        avg = total/ idex 
+        
+        return avg
+    
+    
+    
+    
+
+
+
+
+arr = [3,5,9]
+
+print(avgSum(arr))def addingSum(a,b):
+    return a + b
+
+
+
+addingSum(3,4)
+
+
+
+print(addingSum(4,5))
+
+def avgSum(totalnumAdd):
+    
+    for idex in totalnumAdd:
+        
+        total = sum(totalnumAdd)
+        avg = total/ idex 
+        
+        return avg
+    
+    
+    
+    
+
+
+
+
+arr = [3,5,9]
+
+print(avgSum(arr))
